1b8c6b2d656e110291263613d13566569938ca6a
wjx0820 <1989wjx@gmail.com>
2024-11-28 15:52:52 +0800

    09-EIP6963

--- pages/web3.tsx
+++ pages/web3.tsx
@@ -124,7 +124,13 @@ const CallTest = () => {
 
 export default function Web3() {
   return (
-    <WagmiWeb3ConfigProvider config={config} wallets={[MetaMask()]}>
+    <WagmiWeb3ConfigProvider
+      config={config}
+      wallets={[MetaMask()]}
+      eip6963={{
+        autoAddInjectedWallets: true,
+      }}
+    >
       <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
       <NFTCard
         address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9"
