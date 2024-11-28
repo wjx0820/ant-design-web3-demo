6975218d13a734a16149cc57ca7903c0c4445f43
wjx0820 <1989wjx@gmail.com>
2024-11-28 15:48:23 +0800

    04-CallContract

--- pages/web3.tsx
+++ pages/web3.tsx
@@ -1,24 +1,99 @@
-import { http } from "wagmi";
-import { Address, NFTCard, Connector, ConnectButton } from "@ant-design/web3";
-import { Mainnet, WagmiWeb3ConfigProvider, MetaMask } from '@ant-design/web3-wagmi';
+import { parseEther } from "viem";
+import { Button, message } from "antd";
+import { http, useReadContract, useWriteContract } from "wagmi";
+import {
+  Mainnet,
+  WagmiWeb3ConfigProvider,
+  MetaMask,
+} from "@ant-design/web3-wagmi";
+import {
+  Address,
+  NFTCard,
+  ConnectButton,
+  Connector,
+  useAccount,
+} from "@ant-design/web3";
 
 export default function Web3() {
+  const CallTest = () => {
+    const { account } = useAccount();
+    const result = useReadContract({
+      abi: [
+        {
+          type: "function",
+          name: "balanceOf",
+          stateMutability: "view",
+          inputs: [{ name: "account", type: "address" }],
+          outputs: [{ type: "uint256" }],
+        },
+      ],
+      address: "0xEcd0D12E21805803f70de03B72B1C162dB0898d9",
+      functionName: "balanceOf",
+      args: [account?.address as `0x${string}`],
+    });
+    const { writeContract } = useWriteContract();
+
     return (
-        <WagmiWeb3ConfigProvider
-            chains={[Mainnet]}
-            transports={{
-                [Mainnet.id]: http(),
-            }}
-            wallets={[MetaMask()]}
+      <div>
+        {result.data?.toString()}
+        <Button
+          onClick={() => {
+            writeContract(
+              {
+                abi: [
+                  {
+                    type: "function",
+                    name: "mint",
+                    stateMutability: "payable",
+                    inputs: [
+                      {
+                        internalType: "uint256",
+                        name: "quantity",
+                        type: "uint256",
+                      },
+                    ],
+                    outputs: [],
+                  },
+                ],
+                address: "0xEcd0D12E21805803f70de03B72B1C162dB0898d9",
+                functionName: "mint",
+                args: [BigInt(1)],
+                value: parseEther("0.01"),
+              },
+              {
+                onSuccess: () => {
+                  message.success("Mint Success");
+                },
+                onError: (err) => {
+                  message.error(err.message);
+                },
+              }
+            );
+          }}
         >
-            <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
-            <NFTCard
-                address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9"
-                tokenId={641}
-            />
-             <Connector>
-                <ConnectButton />
-             </Connector>
-        </WagmiWeb3ConfigProvider>
+          mint
+        </Button>
+      </div>
     );
+  };
+
+  return (
+    <WagmiWeb3ConfigProvider
+      chains={[Mainnet]}
+      transports={{
+        [Mainnet.id]: http(),
+      }}
+      wallets={[MetaMask()]}
+    >
+      <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
+      <NFTCard
+        address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9"
+        tokenId={641}
+      />
+      <Connector>
+        <ConnectButton />
+      </Connector>
+      <CallTest />
+    </WagmiWeb3ConfigProvider>
+  );
 }
